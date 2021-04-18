117523cc3eed72b74476346711cde79e964bb04c
Dan Coghlan <coghlada@tcd.ie>
2021-04-18 16:57:54 +0100

    Added Lombok to remove boilerplate code.

--- src/main/java/com/dancoghlan/jspservice/entity/Todo.java
+++ src/main/java/com/dancoghlan/jspservice/entity/Todo.java
@@ -1,8 +1,17 @@
 package com.dancoghlan.jspservice.entity;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.Date;
 
+@EqualsAndHashCode
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "Todo")
 public class Todo {
@@ -22,87 +31,5 @@ public class Todo {
 
     @Column(name = "is_done")
     private boolean isDone;
-
-    public Todo() {
-        // Required by Hibernate
-    }
-
-    public Todo(String userName, String description, Date targetDate, boolean isDone) {
-        this.userName = userName;
-        this.description = description;
-        this.targetDate = targetDate;
-        this.isDone = isDone;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Date getTargetDate() {
-        return targetDate;
-    }
-
-    public void setTargetDate(Date targetDate) {
-        this.targetDate = targetDate;
-    }
-
-    public boolean isDone() {
-        return isDone;
-    }
-
-    public void setDone(boolean isDone) {
-        this.isDone = isDone;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + id;
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        Todo other = (Todo) obj;
-        if (id != other.id) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Todo [id=%s, userName=%s, description=%s, targetDate=%s, isDone=%s]", id, userName, description, targetDate, isDone);
-    }
 
 }
--- src/main/java/com/dancoghlan/jspservice/service/TodoServiceImpl.java
+++ src/main/java/com/dancoghlan/jspservice/service/TodoServiceImpl.java
@@ -2,12 +2,14 @@ package com.dancoghlan.jspservice.service;
 
 import com.dancoghlan.jspservice.entity.Todo;
 import com.dancoghlan.jspservice.repository.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class TodoServiceImpl implements TodoService {
 
     @Autowired
@@ -15,16 +17,19 @@ public class TodoServiceImpl implements TodoService {
 
     @Override
     public List<Todo> getByUser(String userName) {
+        log.info("Retrieving todos for user {}", userName);
         return repository.findAllByUserName(userName);
     }
 
     @Override
     public void add(Todo todo) {
+        log.info("Adding Todo {}", todo);
         repository.save(todo);
     }
 
     @Override
     public void delete(Long id) {
+        log.info("Deleting Todo with id {}", id);
         repository.deleteById(id);
     }
 
